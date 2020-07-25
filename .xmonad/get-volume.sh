44e06c53dcc7e968e492c5089616a1a3cadb42a8
yujideluca <35408668+yujideluca@users.noreply.github.com>
2020-07-25 19:49:15 -0300

    Add files via upload

--- .xmonad/get-volume.sh
+++ .xmonad/get-volume.sh
@@ -1,14 +1,14 @@
 
-vol=`amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1`
+vol=`amixer get Master| grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1`
 mut=`amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep -m 1 off`
-#unmut=`amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep -m 1 on`
-#off="[off]"
-#on="[on]"
+unmut=`amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep -m 1 on`
+off="[off]"
+on="[on]"
 if [[ -z "$mut" ]];
 then
-    echo "Vol:"${vol}"%"
+    echo "Vol:"${vol}
 fi
 if [[ -n "$mut" ]];
 then
-    echo "Vol:"${vol}"%""[Muted]"
+    echo "Vol:"${vol}"[Muted]"
 fi
